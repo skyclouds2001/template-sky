825eb1aeba6e66b88fb503fc19f5581250c4ae9c
skyclouds2001 <skyclouds2001@163.com>
2024-11-05 01:11:17 +0800

    refactor: use class with static fields of template enum

--- src/template.ts
+++ src/template.ts
@@ -1,34 +1,27 @@
 import kleur from 'kleur'
 
 /**
- * template base type
- */
-export interface Template {
-  name: string
-  template: string
-  color: kleur.Color
-}
-
-/**
  * templates enum
  */
-export const templates: Template[] = [
-  {
+export class Template {
+  static readonly 'vite-vue' = {
     name: 'vite-vue',
     template: 'vite-vue-template-sky',
     color: kleur.green,
-  },
-  {
+  } as const
+
+  static readonly 'vite-react' = {
     name: 'vite-react',
     template: 'vite-react-template-sky',
     color: kleur.blue,
-  },
-  {
+  } as const
+
+  static readonly 'rollup' = {
     name: 'rollup',
     template: 'rollup-template-sky',
-    color: kleur.blue,
-  },
-]
+    color: kleur.magenta,
+  } as const
+}
 
 /**
  * validate if a template name is valid and supported one
@@ -36,5 +29,5 @@ export const templates: Template[] = [
  * @returns validation result
  */
 export function isValidTemplateName(name: string): boolean {
-  return Object.values(templates).some((template) => template.name === name.toLowerCase())
+  return Object.values(Template).some((template) => template.name === name.toLowerCase())
 }
